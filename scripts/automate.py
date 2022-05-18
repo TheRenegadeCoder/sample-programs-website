046d194c64b50ce418c65fe974a8b652802b8517
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-05-18 02:00:12 -0400

    Cleaned up dijkstra docs

--- scripts/automate.py
+++ scripts/automate.py
@@ -292,7 +292,7 @@ def generate_projects_index(repo: subete.Repo):
     projects_index_path = pathlib.Path("docs/projects")
     projects_index = snakemd.new_doc("index")
     _generate_front_matter(projects_index, projects_index_path / "front_matter.yaml", "Programming Projects in Every Language")
-    projects_index._contents[-2] = projects_index._contents[-2] + "\nfeatured-image: programming-projects-in-every-language.png"
+    projects_index._contents[-2] = projects_index._contents[-2] + "\nfeatured-image: programming-projects-in-every-language.jpg"
     project_tests = sum(1 if project.has_testing() else 0 for project in repo.approved_projects())
     projects_index.add_paragraph(
         "Welcome to the Projects page! Here, you'll find a list of all of the projects represented in the collection. "
