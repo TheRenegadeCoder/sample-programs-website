960bac429a3b4fe7345916f62b898a19fb659ebf
rzuckerm <rzuckerm@users.noreply.github.com>
2023-02-09 13:06:20 -0600

    Add script to build website locally (#516)
    * Allow developers to build and view webpages locally
    
    * Add info to README
    
    * Update README.md

--- README.md
+++ README.md
@@ -14,3 +14,21 @@ See our list of issues to get started or create your own!
 [language-template]: https://github.com/TheRenegadeCoder/sample-programs-website/blob/master/templates/LANGUAGE_ARTICLE_TEMPLATE.md
 [project-template]: https://github.com/TheRenegadeCoder/sample-programs-website/blob/master/templates/PROJECT_ARTICLE_TEMPLATE.md
 [sample-program-template]: https://github.com/TheRenegadeCoder/sample-programs-website/blob/master/templates/CODE_ARTICLE_TEMPLATE.md
+
+## Building Website Locally
+
+If you wish to build a local copy of the website, create a virtualenv, activate it, and install
+`requirements.txt`:
+
+```console
+virtualenv -p python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+You should only need to do the above once. All other times, just activate the virtualenv with the
+`source` command.
+
+Now, run `generate.sh`. This will build the website, create a temporary web server, and open
+up the home webpage in your default browser at `http://localhost:8000/index.html`. When you are done,
+just press Ctrl+C to exit the web server.
---
+++ generate.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+echo "*** Generate Webpages ***"
+python scripts/automate.py
+
+echo ""
+echo "*** Generate Images ***"
+sources=sources/images/
+images=docs/assets/images/
+logo=icon-small.png
+for file in "$sources"*
+do
+    echo "- Processing ${file}"
+    image-titler --path "$file" --output "$images" --logo "$images$logo"
+    filename=$(basename "$file")
+    edit=$(cd "$images" && ls -t | head -n1)
+    mv "$images$edit" "$images$filename" 
+done
+
+echo ""
+echo "*** Build With Jekyll ***"
+export JEKYLL_VERSION=3.8
+docker run --rm \
+    -e "JEKYLL_UID=$(id -u)" \
+    -e "JEKYLL_GID=$(id -g)" \
+    -v "$PWD/docs:/srv/jekyll:Z" \
+    -w "/srv/jekyll" \
+    -it jekyll/jekyll:$JEKYLL_VERSION \
+    bash -c "bundle install && jekyll build -V --config _config.yml"
+
+echo ""
+echo "*** Change Base URL For Generated Files ***"
+find docs/_site -type f -name '*.html' -exec sed -i 's!https://sampleprograms\.io/!http://localhost:8000/!g' '{}' ';'
+
+echo ""
+echo "*** Start Webserver ***"
+cd docs/_site
+python -m http.server >/dev/null &
+pid=$!
+trap "printf '\n\n*** Kill webserver (PID %s) ***' $pid; kill $pid" SIGINT SIGHUP SIGABRT
+
+echo ""
+echo "*** Open Index ***"
+echo "Press Ctrl+C to exit"
+xdg-open "http://localhost:8000/index.html"
+sleep infinity
