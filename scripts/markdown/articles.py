c94a8b4aa6bbff526fb92c27ab815d5a9052d91b
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-05-18 04:05:49 +0300

    Optimize article generation

--- scripts/markdown/articles.py
+++ scripts/markdown/articles.py
@@ -15,36 +15,40 @@ def add_project_article_section(
 ) -> None:
     """Generates a list of articles for each project page.
 
-    :param snakemd.Document doc: the document to add the section to.
-    :param subete.Repo repo: the repo to pull from.
-    :param subete.Project project: the project to add to the document.
+    Args:
+        doc: The document to add the section to.
+        repo: The subete Repo instance to pull from.
+        project: The target subete Project to find associated language articles for.
+
     """
-    log.info(f"Generating article section of {project}")
+    log.info("Generating article section of %s", project)
     doc.add_heading("Articles", level=2)
+
     articles = []
+    project_name = project.name()
+
     for lang in repo:
         try:
-            program: subete.SampleProgram = lang[project.name()]
+            program = lang[project_name]
         except KeyError:
             continue
 
         program_escaped = markdown_escape(str(program))
-        link = snakemd.Inline(
-            program_escaped,
-            link=program.documentation_url(),
+        articles.append(
+            snakemd.Inline(
+                program_escaped,
+                link=program.documentation_url(),
+            ),
         )
-        articles.append(link)
 
-    num_articles = len(articles)
-    if num_articles > 0:
+    if num_articles := len(articles):
         verb = is_are(num_articles)
         doc.add_paragraph(f"There {verb} {pluralize(num_articles, 'article')}:")
         doc.add_block(snakemd.MDList(articles))
     else:
-        log.warning(f"Failed to find any articles for {project}")
-        doc.add_paragraph(
-            "No articles available. Please consider contributing.",
-        ).insert_link(
+        log.warning("Failed to find any articles for %s", project)
+        paragraph = doc.add_paragraph("No articles available. Please consider contributing.")
+        paragraph.insert_link(
             "Please consider contributing",
             "https://github.com/TheRenegadeCoder/sample-programs-website",
         )
@@ -53,21 +57,24 @@ def add_project_article_section(
 def add_language_article_section(doc: snakemd.Document, repo: subete.Repo, language: str) -> None:
     """Generates a list of articles for each language page.
 
-    :param snakemd.Document doc: the document to add the section to.
-    :param subete.Repo repo: the repo to pull from.
-    :param str language: the language to add to the document in its lookup form (e.g., Python).
+    Args:
+        doc: The document to add the section to.
+        repo: The subete Repo instance to pull from.
+        language: The exact lookup string representation of the targeted language (e.g., "Python").
+
     """
     doc.add_heading("Articles", level=2)
-    num_articles = len(list(repo[language]))
-    verb = is_are(num_articles)
-    doc.add_paragraph(f"There {verb} {pluralize(num_articles, 'article')}:")
 
-    articles = []
-    for program in repo[language]:
-        program_escaped = markdown_escape(str(program))
-        link = snakemd.Inline(
-            program_escaped,
-            link=program._sample_program_doc_url,
+    articles = [
+        snakemd.Inline(
+            markdown_escape(str(program)),
+            link=program.documentation_url(),
         )
-        articles.append(link)
+        for program in repo[language]
+    ]
+
+    num_articles = len(articles)
+    verb = is_are(num_articles)
+
+    doc.add_paragraph(f"There {verb} {pluralize(num_articles, 'article')}:")
     doc.add_block(snakemd.MDList(articles))
