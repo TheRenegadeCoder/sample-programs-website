80cf42dfbf73e66223cd91d6d6321eaa977a0c57
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-05-18 04:09:09 +0300

    Generate "DO NOT EDIT" in a cleaner way

--- scripts/markdown/note.py
+++ scripts/markdown/note.py
@@ -8,24 +8,26 @@ def generate_no_edit_note(
     source_instance: str,
     filenames: list[str],
 ) -> None:
-    """Generates "DO NOT EDIT" note
+    """Generates an embedded raw HTML comment warning against manual edits.
 
-    :param snakemd.Document doc: the document to add the note to.
-    :param str source: the specific source folder to pull from (e.g., languages).
-    :param str source_instance: the specific source instance to pull from (e.g., c-plus-plus).
-    :param list[str] filenames: the markdown filenames
+    Args:
+        doc: The snakemd Document instance to add the note to.
+        source: The specific source folder category (e.g., "languages").
+        source_instance: The specific folder sub-instance (e.g., "c-plus-plus").
+        filenames: A list of structural markdown filenames to track.
+
     """
     note_filenames = "\n".join(
         f"- sources/{source}/{source_instance}/{filename}" for filename in filenames
     )
-    note = f"""\
-<!--
-{AUTO_GEN_NOTE}
 
-Instead, please edit the following:
+    note = (
+        f"<!--\n"
+        f"{AUTO_GEN_NOTE}\n\n"
+        f"Instead, please edit the following:\n\n"
+        f"{note_filenames}\n\n"
+        f"{CONTRIBUTING_NOTE}\n"
+        f"-->"
+    )
 
-{note_filenames}
-
-{CONTRIBUTING_NOTE}
--->"""
     doc.add_raw(note)
