3ecf5bae6244a059d1bcd3b6d311138906608614
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-05-18 06:28:43 +0300

    Backport contextlib.chdir to Python 3.10

--- scripts/generators/tests.py
+++ scripts/generators/tests.py
@@ -1,5 +1,5 @@
-import contextlib
 import logging
+import os
 from pathlib import Path
 
 import glotter
@@ -7,6 +7,23 @@ import subete
 from constants import GENERATED_DIR
 
 log = logging.getLogger(__name__)
+
+from contextlib import AbstractContextManager
+
+
+class chdir(AbstractContextManager):
+    """Non-thread-safe context manager to change the current working directory."""
+
+    def __init__(self, path) -> None:
+        self.path = path
+        self._old_cwd = []
+
+    def __enter__(self) -> None:
+        self._old_cwd.append(os.getcwd())
+        os.chdir(self.path)
+
+    def __exit__(self, *excinfo) -> None:
+        os.chdir(self._old_cwd.pop())
 
 
 def generate_auto_gen_test_docs(repo: subete.Repo) -> None:
@@ -21,9 +38,7 @@ def generate_auto_gen_test_docs(repo: subete.Repo) -> None:
     doc_dir = GENERATED_DIR.resolve()
     repo_dir = Path(repo.sample_programs_repo_dir())
 
-    # Safely switch directories. The context manager guarantees the original
-    # working directory is restored even if an exception occurs inside the block.
-    with contextlib.chdir(repo_dir):
+    with chdir(repo_dir):
         glotter.generate_test_docs(
             doc_dir=doc_dir,
             repo_name="Sample Programs",
